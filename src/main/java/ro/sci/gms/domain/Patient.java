35ad2f026165569d494396777684d04e085f3ae0
Mihai Savin <savin.mihai@gmail.com>
2016-02-06 18:58:39 +0200

    Added patient's profile edit funcionality.

--- src/main/java/ro/sci/gms/domain/Patient.java
+++ src/main/java/ro/sci/gms/domain/Patient.java
@@ -1,12 +1,11 @@
 package ro.sci.gms.domain;
 
-import java.util.Collection;
 import java.util.Date;
 
 public class Patient extends User {
 	private Date dateOfBirth;
 	private Gender gender;
-	private Collection<String> medicalBackground;
+	private String medicalBackground;
 	private Blood bloodType;
 	private Doctor doctor;
 	
@@ -30,10 +29,10 @@ public class Patient extends User {
 	public void setGender(Gender gender) {
 		this.gender = gender;
 	}
-	public Collection<String> getMedicalBackground() {
+	public String getMedicalBackground() {
 		return medicalBackground;
 	}
-	public void setMedicalBackground(Collection<String> medicalBackground) {
+	public void setMedicalBackground(String medicalBackground) {
 		this.medicalBackground = medicalBackground;
 	}
 	public Blood getBloodType() {

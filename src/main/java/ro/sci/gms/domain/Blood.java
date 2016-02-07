b0c3e9690e460be1a2802f682da18fef1f508251
Mihai Savin <savin.mihai@gmail.com>
2016-02-07 14:30:08 +0200

    Updated enums for DB compatibility. [2]

--- src/main/java/ro/sci/gms/domain/Blood.java
+++ src/main/java/ro/sci/gms/domain/Blood.java
@@ -1,19 +1,10 @@
 package ro.sci.gms.domain;
 
 public enum Blood {
-	A, B, AB, O, unspecified;
-
-	public String toString() {
-		// switch this.values(): Did not know how to implement switch
-		// instruction here, so I used the plain old IF
-
-		if (this.equals(A)) return "A";
-		if (this.equals(B)) return "B";	
-		if (this.equals(AB)) return "AB";
-		if (this.equals(O)) return "A";
-		
-		return "U";
-		
-	}
+	A,
+	B,
+	AB,
+	O,
+	unspecified
 
 }
--- src/main/java/ro/sci/gms/domain/Gender.java
+++ src/main/java/ro/sci/gms/domain/Gender.java
@@ -1,8 +1,14 @@
 package ro.sci.gms.domain;
 
 public enum Gender {
+	MALE, FEMALE;
 
-	MALE,
-	FEMALE
-	
+	public String toString() {
+		if (this.equals(MALE)) {
+			return "M";
+		} else {
+			return "F";
+		}
+	}
+
 }
